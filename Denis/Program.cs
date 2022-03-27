c0a09d8b7c8bafa0e878b0566e10636790bef33b
Auchus <100021326+Auchus@users.noreply.github.com>
2022-03-27 20:37:48 +0700

    first commit

---
+++ Denis/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Задача 3*/
+/*        int x1 = 1;
+        int y1 = 2;
+        int x2 = 2;
+        int y2 = 1;
+        bool a = (x1 >= 0) ^ (Math.Pow(y1, 2) != 4);
+        bool b = (x2 * y2 != 0) | (y2 > x2);
+        Console.WriteLine("ответ первого выражения: " + a);
+        Console.WriteLine("ответ второго выражения: " + b);*/
+
+        /*Задача 2*/
+        /*
+                double b = 2.2;
+                double c = 3.7;
+                Console.WriteLine("написать число x");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("написать число a");
+                int a = Convert.ToInt32(Console.ReadLine());
+                double y = 7 * (Math.Exp(Math.Sqrt(Math.Abs(x)))) + Math.Pow(Math.Cos(x),4);
+                double xx = a + Math.Pow(b + c, 3);
+                double aa = Math.Sin(b);
+                Console.WriteLine("ответ 1: " + y);
+                Console.WriteLine("ответ 2: " + xx);
+                Console.WriteLine("ответ 3: " + aa);
+        */
+        /*Задача 1*/
+        /*        Console.WriteLine("написать число y");
+                int y = Convert.ToInt32(Console.ReadLine());
+                double t = Math.Sqrt(Math.Abs(6 * Math.Pow(y, 2) - 0.1 * y + 4));
+                Console.WriteLine("ответ:" + t);*/
+
+
+
+    }
+}
